6bb027eab3e03f3ffccc37ee5d0fe525febbe7fa
bugCats <972245132@qq.com>
2023-09-01 17:33:57 +0800

    响应是Date类型，增加日期格式注解支持

--- cat-client/src/main/java/cc/bugcat/catclient/spi/SimpleResultHandler.java
+++ cat-client/src/main/java/cc/bugcat/catclient/spi/SimpleResultHandler.java
@@ -3,9 +3,13 @@ package cc.bugcat.catclient.spi;
 import cc.bugcat.catclient.beanInfos.CatClientInfo;
 import cc.bugcat.catclient.beanInfos.CatMethodInfo;
 import cc.bugcat.catclient.beanInfos.CatMethodReturnInfo;
+import cc.bugcat.catclient.exception.PayloadResolverException;
 import cc.bugcat.catclient.handler.CatClientContextHolder;
 import cc.bugcat.catclient.exception.CatHttpException;
 import cc.bugcat.catface.spi.AbstractResponesWrapper;
+import cc.bugcat.catface.utils.CatToosUtil;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.lang.reflect.Constructor;
 import java.text.ParsePosition;
@@ -31,7 +35,7 @@ public class SimpleResultHandler implements CatResultProcessor {
 
 
     /**
-     * 打发生http异常时，默认继续抛出
+     * 当发生http异常时，默认继续抛出
      * */
     @Override
     public boolean onHttpError(CatClientContextHolder context) throws Throwable {
@@ -48,11 +52,7 @@ public class SimpleResultHandler implements CatResultProcessor {
             return null;
         }
         CatMethodInfo methodInfo = context.getMethodInfo();
-        CatClientInfo clientInfo = context.getClientInfo();
-        CatPayloadResolver resolver = context.getFactoryAdapter().getPayloadResolver();
         CatMethodReturnInfo returnInfo = methodInfo.getReturnInfo();
-        AbstractResponesWrapper wrapperHandler = clientInfo.getWrapperHandler();
-
         Class returnClass = returnInfo.getClazz();
         
         // 如果响应类型是Object，那么直接返回原始的字符串，不做任何处理
@@ -60,16 +60,20 @@ public class SimpleResultHandler implements CatResultProcessor {
             return result;
         }
 
+        CatClientInfo clientInfo = context.getClientInfo();
+        CatPayloadResolver resolver = context.getFactoryAdapter().getPayloadResolver();
+        AbstractResponesWrapper wrapperHandler = clientInfo.getWrapperHandler();
+        
         // 没有设置包装器类
         if ( wrapperHandler == null ) {
             if( void.class.equals(returnClass) ){
                 return null;
-            } else  if(returnInfo.isSimple()){ //日期、基本数据类型、及包装类
+            } else if(returnInfo.isSimple()){ //日期、基本数据类型、及包装类
                 return toSimpleBean(result, returnInfo);
-            } else if ( java.sql.Date.class.isAssignableFrom(returnInfo.getClazz()) ){
-                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mi:ss.SSS");   //非线程安全
-                ParsePosition pos = new ParsePosition(0);
-                Date strtodate = formatter.parse(result, pos);
+            } else if (Date.class.isAssignableFrom(returnInfo.getClazz())){
+                String dateFormat = getDateFormat(context);
+                SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+                Date strtodate = formatter.parse(result);
                 return strtodate;
             } else {//复杂对象
                 return resolver.toJavaBean(result, returnInfo.getType());
@@ -90,34 +94,55 @@ public class SimpleResultHandler implements CatResultProcessor {
 
 
     @Override
-    public Object onFinally(Object resp, CatClientContextHolder context) throws Exception {
+    public Object onFinally(Object result, CatClientContextHolder context) throws Exception {
         CatClientInfo clientInfo = context.getClientInfo();
         CatMethodInfo methodInfo = context.getMethodInfo();
 
         AbstractResponesWrapper wrapperHandler = clientInfo.getWrapperHandler();
-        if ( wrapperHandler == null || resp == null ) {
-            return resp;
+        if ( wrapperHandler == null || result == null ) {
+            return result;
         }
         Class wrapperClass = wrapperHandler.getWrapperClass();
         Class returnClass = methodInfo.getReturnInfo().getClazz();
 
         //方法的响应，与包装器类型相同，直接返回对象
         if( wrapperClass.equals(returnClass) ){
-            return resp;
-        } else if( wrapperClass.isAssignableFrom(resp.getClass()) ){
+            return result;
+        } else if( wrapperClass.isAssignableFrom(result.getClass()) ){
             // 方法的响应与包装器类型不同相同，并且响应类型是包装器类：拆包裹、校验
-            wrapperHandler.checkValid(resp);
-            return wrapperHandler.getValue(resp);
+            wrapperHandler.checkValid(result);
+            return wrapperHandler.getValue(result);
         }else {
-            return resp;
+            return result;
         }
     }
 
+    /**
+     * 获取方法上关于Date的格式
+     * */
+    protected String getDateFormat(CatClientContextHolder context){
+        String format = null;
+        CatMethodInfo methodInfo = context.getMethodInfo();
+        JsonFormat jsonFormat = methodInfo.findAnnotation(JsonFormat.class);
+        if ( jsonFormat != null ) {
+            format = jsonFormat.pattern();
+        }
+        if ( CatToosUtil.isBlank(format) ){
+            JSONField jsonField = methodInfo.findAnnotation(JSONField.class);
+            if( jsonField != null ){
+                format = jsonField.format();
+            }
+        }
+        if ( CatToosUtil.isBlank(format) ){
+            format = "yyyy-mm-dd HH:mi:ss.SSS";
+        }
+        return format;
+    }
 
     /**
      * 将 String 强制转换 clazz对应的简单对象
-     */
-    public static Object toSimpleBean(String text, CatMethodReturnInfo returnInfo) {
+     * */
+    public Object toSimpleBean(String text, CatMethodReturnInfo returnInfo) {
         String returnName = returnInfo.getName();
         if( "STRING".equals(returnName) ) {
             return text;
@@ -130,16 +155,16 @@ public class SimpleResultHandler implements CatResultProcessor {
         } else {
             try {
                 return returnInfo.getClazz().cast(text);
-            } catch (Exception e) {
-                return null;
+            } catch (Exception ex) {
+                throw new PayloadResolverException("[" + text + "]转" + returnInfo.getClazz().getName() + "异常", ex);
             }
         }
     }
 
     /**
      * 得到Number类型的具体子类
-     */
-    public static Object getNumberType(String text, String className, Class clazz) {
+     * */
+    public Object getNumberType(String text, String className, Class clazz) {
         if( "".equals(text) ){
             return null;
         }
@@ -161,9 +186,10 @@ public class SimpleResultHandler implements CatResultProcessor {
             try {
                 Constructor constructor = clazz.getConstructor(String.class);
                 return constructor.newInstance(text);
-            } catch ( Exception e ) { }
+            } catch ( Exception ex ) {
+                throw new PayloadResolverException("[" + text + "]转" + className + "异常", ex);
+            }
         }
-        return text;
     }
 
 }
